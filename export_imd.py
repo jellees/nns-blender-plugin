45801836b8b4bdc2d301c47d3226f515c95c3d4f
Jesse64 <jelle.streekstra@hotmail.com>
2019-08-24 18:32:26 +0200

    Replace old code with new

--- export_imd.py
+++ export_imd.py
@@ -8,363 +8,13 @@ from . import nitro_model
 settings = None
 
 
-"""
-Display list collection.
-
-model_data = {
-    output: {
-        polygon_size: 0,
-        triangle_size: 0,
-        quad_size: 0,
-        vertex_size: 0
-    },
-    polygons: [
-        {
-            material: 0,
-            primitives: [
-                {
-                    type: triangles,
-                    vertex_size: 0,
-                    commands: [
-                        {
-                            type: "mtx",
-                            tag: "idx",
-                            data: "0"
-                        },
-                        {
-                            type: "pos_xyz",
-                            tag: 'xyz',
-                            data: "0.141357 0.119873 -0.139404"
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-}
-"""
-model_data = {}
-
-
-# FX32 Conversion Functions
-def float_to_fx32(value):
-    return int(round(value * 4096))
-
-
-def fx32_to_float(value):
-    return float(value) / 4096
-
-
-def vector_to_vecfx32(vec):
-    return [
-        float_to_fx32(vec.x),
-        float_to_fx32(vec.y),
-        float_to_fx32(vec.z)
-    ]
-
-
-def vecfx32_to_vector(values):
-    return Vector([
-        fx32_to_float(values[0]),
-        fx32_to_float(values[1]),
-        fx32_to_float(values[2]),
-    ])
-
-
-def calculate_diff(vec1, vec2):
-    return [
-        vec1[0] - vec2[0],
-        vec1[1] - vec2[1],
-        vec1[2] - vec2[2],
-    ]
-
-
-# Bound Functions
-def get_object_max_min(obj):
-    bounds = [obj.matrix_world @ Vector(v) for v in obj.bound_box]
-    return {
-        'min': bounds[0],
-        'max': bounds[6]
-    }
-
-
-def get_all_max_min():
-    min_p = Vector([float('inf'), float('inf'), float('inf')])
-    max_p = Vector([-float('inf'), -float('inf'), -float('inf')])
-    for obj in bpy.data.objects:
-        if obj.type != 'MESH':
-            continue
-        max_min = get_object_max_min(obj)
-        # Max
-        max_p.x = max(max_p.x, max_min['max'].x)
-        max_p.y = max(max_p.y, max_min['max'].y)
-        max_p.z = max(max_p.z, max_min['max'].z)
-        # Min
-        min_p.x = min(min_p.x, max_min['min'].x)
-        min_p.y = min(min_p.y, max_min['min'].y)
-        min_p.z = min(min_p.z, max_min['min'].z)
-    return {
-        'min': min_p,
-        'max': max_p
-    }
-
-
-# Pos Scale Functions
-def calculate_pos_scale(max_coord):
-    m = float_to_fx32(max_coord)
-    pos_scale = 0
-    while m >= 0x8000:
-        pos_scale += 1
-        m >>= 1
-    return pos_scale
-
-
-def get_pos_scale():
-    # It's done twice, maybe we can call this once and save it somewhere.
-    max_min = get_all_max_min()
-    max_max = abs(max(max_min['max'].x, max_min['max'].y, max_min['max'].z))
-    min_min = abs(min(max_min['min'].x, max_min['min'].y, max_min['min'].z))
-    max_coord = max(max_max, min_min)
-    return calculate_pos_scale(max_coord)
-
-
-def get_box_test():
-    # It's done twice, maybe we can call this once and save it somewhere.
-    max_min = get_all_max_min()
-    return {
-        'xyz': max_min['min'],
-        'whd': max_min['max'] - max_min['min']
-    }
-
-
-def get_box_test_pos_scale(box):
-    max_whd = abs(max(box['whd'].x, box['whd'].y, box['whd'].z))
-    min_xyz = abs(min(box['xyz'].x, box['xyz'].y, box['xyz'].z))
-    max_coord = max(max_whd, min_xyz)
-    return calculate_pos_scale(max_coord)
-
-
-def apply_pos_scale_on_vector(vec, pos_scale):
-    return Vector([
-        fx32_to_float(float_to_fx32(vec.x) >> pos_scale),
-        fx32_to_float(float_to_fx32(vec.y) >> pos_scale),
-        fx32_to_float(float_to_fx32(vec.z) >> pos_scale),
-    ])
-
-
-def apply_pos_scale_on_vecfx32(vecfx32, pos_scale):
-    return [
-        vecfx32[0] >> pos_scale,
-        vecfx32[1] >> pos_scale,
-        vecfx32[2] >> pos_scale
-    ]
-
-
-# Vertex command check functions
-def is_pos_s(vecfx32):
-    return (
-        (vecfx32[0] & 0x3F) == 0 and
-        (vecfx32[1] & 0x3F) == 0 and
-        (vecfx32[2] & 0x3F) == 0
-    )
-
-
-def is_pos_diff(diff):
-    # 512 is 0.125 in FX32
-    return (
-        abs(diff[0]) < 512 and
-        abs(diff[1]) < 512 and
-        abs(diff[2]) < 512
-    )
-
-
-# Vertex command append functions
-def append_pos_xyz(commands, vec):
-    floats = [str(v) for v in vec]
-
-    commands.append(
-        {
-            'type': 'pos_xyz',
-            'tag': 'xyz',
-            'data': ' '.join(floats)
-        }
-    )
-
-
-def append_pos_s(commands, vec):
-    floats = [str(v) for v in vec]
-
-    commands.append(
-        {
-            'type': 'pos_s',
-            'tag': 'xyz',
-            'data': ' '.join(floats)
-        }
-    )
-
-
-def append_pos_diff(commands, vec):
-    floats = [str(v) for v in vec]
-
-    commands.append(
-        {
-            'type': 'pos_diff',
-            'tag': 'xyz',
-            'data': ' '.join(floats)
-        }
-    )
-
-
-def append_pos_yz(commands, vec):
-    floats = [str(v) for v in [vec.y, vec.z]]
-
-    commands.append(
-        {
-            'type': 'pos_yz',
-            'tag': 'yz',
-            'data': ' '.join(floats)
-        }
-    )
-
-
-def append_pos_xz(commands, vec):
-    floats = [str(v) for v in [vec.x, vec.z]]
-
-    commands.append(
-        {
-            'type': 'pos_xz',
-            'tag': 'xz',
-            'data': ' '.join(floats)
-        }
-    )
-
-
-def append_pos_xy(commands, vec):
-    floats = [str(v) for v in [vec.x, vec.y]]
-
-    commands.append(
-        {
-            'type': 'pos_xy',
-            'tag': 'xy',
-            'data': ' '.join(floats)
-        }
-    )
-
-
-def get_material_index(obj, index):
-    name = obj.material_slots[index].material.name
-    return bpy.data.materials.find(name)
-
-
-def get_primitive(primitives, tp):
-    for primitive in primitives:
-        if primitive['type'] == tp:
-            return primitive
-    primitives.append(
-        {
-            'type': tp,
-            'vertex_size': 0,
-            'commands': []
-        }
-    )
-    return primitives[-1]
-
-
-# Will be changed later
-global_vtx_count = 0
-previous_fx32 = []
-
-
-def polygon_to_primitive(dl, obj, polygon):
-    # Will be changed later
-    global global_vtx_count
-    global previous_fx32
-
-    verts_local = [v.co for v in obj.data.vertices.values()]
-    verts_world = [obj.matrix_world @ v_local for v_local in verts_local]
-
-    if len(polygon.vertices) == 3:
-        primitive = get_primitive(dl['primitives'], 'triangles')
-        primitive['vertex_size'] += 3
-        model_data['output']['vertex_size'] += 3
-        model_data['output']['triangle_size'] += 1
-    elif len(polygon.vertices) == 4:
-        primitive = get_primitive(dl['primitives'], 'quads')
-        primitive['vertex_size'] += 4
-        model_data['output']['vertex_size'] += 4
-        model_data['output']['quad_size'] += 1
-
-    for i in polygon.vertices:
-        pos_scale = model_data['pos_scale']
-
-        # Get vertex and convert it to VecFx32
-        vertex = verts_world[i]
-        vecfx32 = vector_to_vecfx32(Vector(vertex))
-
-        # Apply pos_scale
-        scaled_fx32 = apply_pos_scale_on_vecfx32(vecfx32, pos_scale)
-        scaled_vec = vecfx32_to_vector(scaled_fx32)
-
-        # Calculate difference from previous vertex
-        if global_vtx_count > 0:
-            diff_fx32 = calculate_diff(scaled_fx32, previous_fx32)
-            diff_vec = vecfx32_to_vector(diff_fx32)
-
-        # PosYZ
-        if global_vtx_count > 0 and diff_fx32[0] == 0:
-            append_pos_yz(primitive['commands'], scaled_vec)
-        # PosXZ
-        elif global_vtx_count > 0 and diff_fx32[1] == 0:
-            append_pos_xz(primitive['commands'], scaled_vec)
-        # PosXY
-        elif global_vtx_count > 0 and diff_fx32[2] == 0:
-            append_pos_xy(primitive['commands'], scaled_vec)
-        # PosDiff
-        elif global_vtx_count > 0 and is_pos_diff(diff_fx32):
-            append_pos_xz(primitive['commands'], diff_vec)
-        # PosShort
-        elif is_pos_s(scaled_fx32):
-            append_pos_s(primitive['commands'], scaled_vec)
-        # PosXYZ
-        else:
-            append_pos_xyz(primitive['commands'], scaled_vec)
-
-        # Update previous position
-        previous_fx32 = scaled_fx32
-        global_vtx_count += 1
-
-
-def prepare_model_data():
-    global model_data
-    model_data = {
-        'pos_scale': get_pos_scale(),
-        'output': {
-            'polygon_size': len(bpy.data.materials),
-            'triangle_size': 0,
-            'quad_size': 0,
-            'vertex_size': 0
-        },
-        'polygons': []
-    }
-
-    for i in range(len(bpy.data.materials)):
-        model_data['polygons'].append({'material': i, 'primitives': []})
-
-    for obj in bpy.data.objects:
-        if obj.type != 'MESH':
-            continue
-        for polygon in obj.data.polygons:
-            index = get_material_index(obj, polygon.material_index)
-            dl = model_data['polygons'][index]
-            polygon_to_primitive(dl, obj, polygon)
-
-    with open(settings['filepath'] + '.json', 'w') as f:
-        f.write(json.dumps(model_data, indent=4))
+model = None
 
 
 def generate_model_info(imd):
     model_info = ET.SubElement(imd, 'model_info')
-    model_info.set('pos_scale', str(model_data['pos_scale']))
+    pos_scale = model.boundry_box.get_pos_scale()
+    model_info.set('pos_scale', str(pos_scale))
     model_info.set('scaling_rule', 'standard')
     model_info.set('vertex_style', 'direct')
     model_info.set('magnify', '1.000000')
@@ -380,13 +30,13 @@ def generate_model_info(imd):
 
 
 def generate_box_test(imd):
-    box = get_box_test()
-    pos_scale = get_box_test_pos_scale(box)
+    box = model.boundry_box.get_box_test()
+    pos_scale = model.boundry_box.get_box_test_pos_scale()
     box_test = ET.SubElement(imd, 'box_test')
     box_test.set('pos_scale', str(pos_scale))
-    floats = [str(v) for v in apply_pos_scale_on_vector(box['xyz'], pos_scale)]
+    floats = [str(v) for v in nitro_model.apply_pos_scale_on_vector(box['xyz'], pos_scale)]
     box_test.set('xyz', ' '.join(floats))
-    floats = [str(v) for v in apply_pos_scale_on_vector(box['whd'], pos_scale)]
+    floats = [str(v) for v in nitro_model.apply_pos_scale_on_vector(box['whd'], pos_scale)]
     box_test.set('whd', ' '.join(floats))
 
 
@@ -439,7 +89,7 @@ def generate_matrices(imd):
 def generate_polygons(imd):
     polygons = ET.SubElement(imd, 'polygons')
 
-    for index, dl in enumerate(model_data['polygons']):
+    for index, dl in enumerate(model.polygons):
         polygon = ET.SubElement(polygons, 'polygon')
         polygon.set('index', str(index))
         polygon.set('name', f'polygon{index}')
@@ -452,33 +102,31 @@ def generate_polygons(imd):
         mtx_list.text = '0'
 
         primitive_array = ET.SubElement(mtx_prim, 'primitive_array')
-        primitive_array.set('size', str(len(dl['primitives'])))
+        primitive_array.set('size', str(len(dl.primitives)))
 
-        for pr in dl['primitives']:
+        for pr in dl.primitives:
             primitive = ET.SubElement(primitive_array, 'primitive')
             primitive.set('index', '0')
-            primitive.set('type', pr['type'])
-            primitive.set('vertex_size', str(pr['vertex_size']))
-            for cmd in pr['commands']:
-                command = ET.SubElement(primitive, cmd['type'])
-                command.set(cmd['tag'], cmd['data'])
+            primitive.set('type', pr.type)
+            primitive.set('vertex_size', str(pr.vertex_size))
+            for cmd in pr.commands:
+                command = ET.SubElement(primitive, cmd.type)
+                command.set(cmd.tag, cmd.data)
 
 
 def generate_output_info(imd):
-    output = model_data['output']
+    output = model.output_info
     output_info = ET.SubElement(imd, 'output_info')
-    output_info.set('vertex_size', str(output['vertex_size']))
-    output_info.set('polygon_size', str(output['polygon_size']))
-    output_info.set('triangle_size', str(output['triangle_size']))
-    output_info.set('quad_size', str(output['quad_size']))
+    output_info.set('vertex_size', str(output.vertex_size))
+    output_info.set('polygon_size', str(output.polygon_size))
+    output_info.set('triangle_size', str(output.triangle_size))
+    output_info.set('quad_size', str(output.quad_size))
 
 
 def generate_body(imd, export_settings):
-    global settings
+    global settings, model
     settings = export_settings
     model = nitro_model.get_nitro_model()
-
-    prepare_model_data()
 
     generate_model_info(imd)
     generate_box_test(imd)
